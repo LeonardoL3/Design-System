f300e079e13d91027a6b0a57f1280fcea760919e
Leonardo <lelazzati7@gmail.com>
2022-11-04 09:13:14 -0300

    feat: add Checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Box, Text, Checkbox, CheckboxProps } from "@vortex-ui/react";
+
+export default {
+  title: "Form/Checkbox",
+  component: Checkbox,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          htmlFor="input-example"
+          css={{ display: "flex", alignItems: "center", gap: "$2" }}
+        >
+          {Story()}
+          <Text size="sm"> Accept terms of use </Text>
+        </Box>
+      );
+    },
+  ],
+} as Meta<CheckboxProps>;
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {
+    placeholder: "Add any observations...",
+  },
+};
